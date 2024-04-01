1d3010c199336d3042673568191a78e4456a54d5
turtle0617 <qqaz50313@gmail.com>
2024-04-01 17:15:18 +0800

    fix: Add base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/conf ig/
 export default defineConfig({
+  base: '/travel-questions/',
   plugins: [react()],
 })
